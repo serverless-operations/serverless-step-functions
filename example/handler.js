21cf34a0bdf93fc163baae913880b3c0d3931562
Chris <chris.zirkel@gmail.com>
2021-07-13 14:02:37 +0200

    feat: example sls project

---
+++ example/handler.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.hello = async event => ({
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    },
+    null,
+    2,
+  ),
+});
