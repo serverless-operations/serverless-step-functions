cba07619757e8cb70da8759f9824101beffe952c
horike37 <horike@serverless.co.jp>
2021-12-21 11:10:58 +0900

    fix: security update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  "extends": "airbnb",
-  "plugins": [],
-  "rules": {
-    "func-names": "off",
-    "prefer-destructuring": "off",
-    "no-plusplus": "off",
-    "no-template-curly-in-string": "off",
-    "no-restricted-syntax": "off",
-    "strict": "off",
-    "prefer-rest-params": "off",
-    "react/require-extension" : "off",
-    "import/no-extraneous-dependencies" : "off"
+  extends: 'airbnb',
+  plugins: [],
+  rules: {
+    'func-names': 'off',
+    'prefer-destructuring': 'off',
+    'no-plusplus': 'off',
+    'no-template-curly-in-string': 'off',
+    'no-restricted-syntax': 'off',
+    strict: 'off',
+    'prefer-rest-params': 'off',
+    'react/require-extension': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
-  "env": {
-       "mocha": true
-   }
+  env: {
+    mocha: true,
+  },
 };
